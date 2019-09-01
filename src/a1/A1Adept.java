0510eabce24813280ea86a9c2e8875ca821e96f5
hhp12 <hhp12@windows10.microdone.cn>
2019-09-01 18:10:58 -0400

    1

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -7,8 +7,67 @@ public class A1Adept {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		int a = scan.nextInt();
+		String[] name = null;
 		
+		double[] amount = null;
+		
+		String[] goodName=null;
+		double[] goodPrice=null;
+		for (int i = 0; i < a; i++) {
+			goodName [i] = scan.next();
+			goodPrice[i]= scan.nextDouble();
+	    }
+		int d = scan.nextInt();
+		for (int z = 0; z < d; z++) {
+			String n = scan.next();
+			String m = scan.next();
+			String combined = n + " " + m;
+			name[z] = combined;
+			
+			int x = scan.nextInt();
+			double individual = 0;
+			for (int k = 0; k < x; k++) {
+				int number = scan.nextInt();
+				String food = scan.next();
+				for (int i = 0; i < goodName.length; i++) {
+					if (food.contentEquals(goodName[i])) {
+						individual += number*goodPrice[i];
+					}
+				}
+				
+			}
+		amount[z] = individual;
+		}
+		double max = amount[0];
+		double min = amount[0];
+		double average = 0;
+		for (int i = 0; i < amount.length; i++) {
+			average += amount[i];
+			if (amount[i] > max) {
+				max = amount[i];
+			}
+			if (amount[i] < min) {
+				min = amount[i];
+			}
+		}
+		average = average/d;
+		String maxName = "";
+		String minName = "";
+		for (int i = 0; i < amount.length; i++) {
+			if (amount[i] == max) {
+				maxName = name[i];
+				
+			}
+			if (amount[i] == min) {
+				minName = name[i];
+				
+			}
+				
+		}
+		
+		System.out.println("Biggest:" + " " + maxName + "(" + max + ")");
+		System.out.println("Smallest:" + " " + minName + "(" + min + ")");
+		System.out.println("Average:" + " " + average);
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,20 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
-		
+		int a=scan.nextInt();
+		for (int i = 0; i < a; i++) {
+			String b=scan.next();
+			String c=scan.next();
+			double d=scan.nextInt();
+			double total=0;
+			for (int z = 0; z < d; z++) {
+				int e=scan.nextInt();
+				String f=scan.next();
+				double g=scan.nextDouble();
+				double h = e*g;
+				total +=h;
+			}
+			System.out.println(b.substring(0, 1) + "." + " " + c + " " + total);
+		}
 	}
 }
